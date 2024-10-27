5ccd46f636f657e47752d63dfee98513f07c62e5
= <bottenbergdave@gmail.com>
2024-10-27 19:39:23 -0400

    created new controller function

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -4,5 +4,16 @@ const prisma = new PrismaClient();
 module.exports= {
     getPosts: async(req,res) => {
         res.send("hello world");
+    },
+    createUser: async(req,res)=> {
+        const username = req.body.username;
+        const password= req.body.password;
+        const name = req.body.name;
+        const email= req.body.email;
+
+        const newUser = await prisma.user.create({
+            data:{email,name,username,password}
+        });
+        res.redirect('http://localhost:5173/login');
     }
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,56 @@
-const expres = require('express');
-const router= express.Router();
-const {PrismaClient} = require('@prisma/client');
+const express = require("express");
+const router = express.Router();
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const controller = require('../controllers/apiController');
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require('bcryptjs');
+const controller = require("../controllers/apiController");
 
-router.get('/', controller.getPosts);
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    const user = await prisma.user.findFirst({
+      where: {
+        username,
+      },
+    });
+    if (!user) {
+      return done(null, false, { message: "incorrect username" });
+    }
+    const match= await bcrypt.compare(password,user.password);
+    if (!match) {
+      return done(null, false, { message: "incorrect password" });
+    }
+    return done(null, user);
+  })
+);
+passport.serializeUser(async (id, done) => {
+  done(null, user, id);
+});
+passport.deserializeUser(async (id, done) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      id,
+    },
+  });
+  done(null, user);
+});
+router.use(
+  session({ secret: "ilovemorgan", resave: false, saveUninitialized: false })
+);
+router.use(passport.session());
 
-module.exports= router;
+router.post('/',controller.createUser);
+router.post('/log-in', 
+    passport.authenticate('local', {
+        successRedirect: 'http://localhost:5173/posts',
+        failureRedirect: 'http://localhost:5173/register'
+    })
+);
+router.get("/", controller.getPosts);
+
+module.exports = router;
+
+//figure out why I cant authenticate user
+//use a main.js file to look into my prisma schema manually to check output

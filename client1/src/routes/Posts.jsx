63c619bb09b0ef544c5dad7910c0a5b37147b854
= <bottenbergdave@gmail.com>
2024-10-28 08:50:07 -0400

    created a posts route that the user will need to be authorized/authenticated to access

---
+++ client1/src/routes/Posts.jsx
@@ -0,0 +1,13 @@
+const Posts = () => {
+
+
+    return (
+        <div>
+            Hello World from the posts section. 
+            Only authenticated and authorized users will be able to access
+            and comment on mojos blog posts....
+        </div>
+    )
+}
+
+export default Posts;
